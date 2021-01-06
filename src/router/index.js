acf63d62b0d61f92c846c602a9ba5d4c13d45aa3
aq_minato.naka <minato.naka@asia-quest.jp>
2021-01-07 07:32:47 +0900

    ルーティング一覧追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,69 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import SignUp from "../views/SignUp.vue";
+import SignIn from "../views/SignIn.vue";
+import CategoryIndex from "../views/category/Index.vue";
+import CategoryCreate from "../views/category/Create.vue";
+import CategoryEdit from "../views/category/Edit.vue";
+import TaskIndex from "../views/task/Index.vue";
+import TaskCreate from "../views/task/Create.vue";
+import TaskEdit from "../views/task/Edit.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    redirect: { name: "CategoryIndex" },
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/signup",
+    name: "SignUp",
+    component: SignUp,
+  },
+  {
+    path: "/signin",
+    name: "SignIn",
+    component: SignIn,
+  },
+  {
+    path: "/categories",
+    name: "CategoryIndex",
+    component: CategoryIndex,
+  },
+  {
+    path: "/categories/create",
+    name: "CategoryCreate",
+    component: CategoryCreate,
+  },
+  {
+    path: "/categories/:categoryId/edit",
+    name: "CategoryEdit",
+    component: CategoryEdit,
+  },
+  {
+    path: "/categories/:categoryId",
+    component: TaskIndex,
+    children: [
+      {
+        path: "tasks",
+        name: "TaskIndex",
+        component: TaskIndex,
+      },
+      {
+        path: "tasks/create",
+        name: "TaskCreate",
+        component: TaskCreate,
+      },
+      {
+        path: "tasks/:taskId/edit",
+        name: "TaskEdit",
+        component: TaskEdit,
+      },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
